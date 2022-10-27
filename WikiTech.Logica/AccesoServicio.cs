dbe8c1b295f99ff38b6c9688c2a5592d2cb074bf
ScarletCortes <scortesvaldes@alumno.unlam.edu.ar>
2022-10-27 01:47:30 -0300

    Agregar archivos de proyecto.

---
+++ WikiTech.Logica/AccesoServicio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikiTech.Entidades;
+
+namespace WikiTech.Logica
+{
+
+    public interface IAccesoServicio
+    {
+        bool Login(Usuario usuario);
+
+        void Registrarse(Usuario usuario);
+    }
+
+    public class AccesoServicio : IAccesoServicio
+    {
+        public bool Login(Usuario usuario)
+        {
+            return true;
+        }
+
+        public void Registrarse(Usuario usuario)
+        {
+            
+        }
+    }
+}
---
+++ WikiTech.Web/Controllers/AccesoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WikiTech.Entidades;
+using WikiTech.Logica;
+
+namespace WikiTech.Web.Controllers
+{
+    public class AccesoController : Controller
+    {
+
+        public IAccesoServicio _IAccesoServicio;
+        
+        public AccesoController(IAccesoServicio _accesoServicio)
+        {
+            _IAccesoServicio = _accesoServicio;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _IAccesoServicio.Login(usuario);
+
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                return View(usuario);
+            }
+           
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+    }
+}
